2efcb603b16281e107d7a790b34431f4dfb7cf34
José Deodanes-code <60304928+Jose-developer-start@users.noreply.github.com>
2021-03-31 07:25:10 +0200

    Add files via upload

---
+++ crud/vistas/table.php
@@ -0,0 +1,21 @@
+<table class="table">
+  <thead class="thead-dark">
+    <tr>
+      <th scope="col">Cod</th>
+      <th scope="col">Email</th>
+      <th scope="col">Contraseña</th>
+      <th scope="col">Acciones</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>
+        <a class="btn btn-danger" href="">Eliminar</a>
+        <a class="btn btn-primary" href="">Actualizar</a>
+    </td>
+    </tr>
+  </tbody>
+</table>
